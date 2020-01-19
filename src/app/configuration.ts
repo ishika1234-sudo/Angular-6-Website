fcc0d46225560dedb80fadc38383e305e659f644
ishika1234-sudo <riya.b1997@gmail.com>
2020-01-19 20:55:07 +0530

    first commit

---
+++ src/app/configuration.ts
@@ -0,0 +1,124 @@
+export const configuration = {
+     header:{
+    heading:'My website',
+    headingtext:'This is my first website in angularjs using node js and angular cli for creating apps',
+    buttontext:'do some action',
+    buttonlink:'\home'
+     },
+     intro:
+     {
+       tagline:'SUCCESS',
+       title:'How words can become feelings through poetry',
+       description:'Poetry is a style of writing that uses a formal organization and that is often divided up into lines or stanzas, or it refers to something beautiful. ',
+       features:
+      [
+        {icons:'html5',title:'Awards',
+          description:'Awarded annually, the $100,000 Ishika Poetry Prize honors a living Indian poet whose accomplishments warrant extraordinary recognition. Established in 2000 by Ishika Bhattacharya, the prize is one of the most prestigious awards given to Indian poets.'},
+       
+        {icons:'bolt',title:'Media partnerships',
+          description:'Through a variety of collaborations, the Foundation invests in poetry programming to reach the Indian public across the full media spectrum. Over time, the Poetry Foundation aims to achieve a more robust presence for poetry-related stories in print, radio, television, film, and social media.'} , 
+     
+        {icons:'tablet',title:'Heart out loud',
+          description:'Poetry Out Loud helps students master public speaking skills, build self-confidence, and learn about literary history and contemporary life. Created by the National Endowment for the Arts and the Poetry Foundation in 2005, Heart Out Loud is administered in partnership with the State Arts Agencies of all 50 state.'},
+     
+        {icons:'rocket',title:'People',
+          description:'We can trace the roots of our organization back a century into the history of India, to the origins of Poetry magazine and to its founder Harriet Monroe. It is her vision that informs the work we do every day. But there is also a second woman—really a second founder—whose philanthropy has made everything the Foundation does possible. '}
+      ]
+   
+     },
+     services:
+     {
+       tagline:'BELIEVING',
+       title:'Focusing On What Matters Most',
+       description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam!'
+   
+     },
+     testimonials:
+     {
+       tagline:'FEEDBACK',
+       title:'What our customers are saying',
+       description:'',
+       feedbacks:[
+         {name:'John Doe ', userimage:'user-1.jpg', comments:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua', designation:'Happy Customer'},
+         {name:'Roslyn Doe', userimage:'user-2.jpg', comments:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua', designation:'Happy Customer'},
+         {name:'Thomas Doe', userimage:'user-3.jpg', comments:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua', designation:'Happy Customer'},
+       ]
+   
+     },
+     clients:
+     {
+       tagline:'TRUST',
+       title:'Companies who use our services',
+       description:'LLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam!',
+       companies:[
+        {name:'Tree', weblink:'company-logo1.png', logo:'company-logo1.png'},
+        {name:'Fingerprint', weblink:'company-logo2.png', logo:'company-logo2.png'},
+        {name:'The Man', weblink:'company-logo3.png', logo:'company-logo3.png'},
+        {name:'Mustache', weblink:'company-logo4.png', logo:'company-logo3.png'},
+        {name:'Goat', weblink:'company-logo5.png', logo:'company-logo3.png'},
+        {name:'Justice', weblink:'company-logo6.png', logo:'company-logo3.png'},
+        {name:'Ball', weblink:'company-logo7.png', logo:'company-logo3.png'},
+        {name:'Cold', weblink:'company-logo8.png', logo:'company-logo3.png'},
+        {name:'Cold', weblink:'company-logo9.png', logo:'company-logo3.png'}
+            ]
+     },
+     pricing:
+     {
+       tagline:'YOUR CHOICE',
+       title:'We have the right package for you',
+       description:'',
+       plans:[
+        {title:'PERSONAL', subtitle:'The standard version', desc:'Lorem ipsum dolor sit amet, consectetur adipiscing elit', price:'19', currency:'$', 
+          downloads:'5 Downloads', extensions:'2 Extensions', tutorials:'Tutorials', forum:'Forum Support', updates:'1 year free updates',buttontest:'buy now', buttonlink:'#', featured:true} ,
+       
+        {title:'PERSONAL', subtitle:'The standard version', desc:'Lorem ipsum dolor sit amet, consectetur adipiscing elit', price:'19', currency:'$', 
+          downloads:'5 Downloads', extensions:'2 Extensions', tutorials:'Tutorials', forum:'Forum Support', updates:'1 year free updates',buttontest:'buy now', buttonlink:'#', featured:true} ,
+        
+        {title:'PERSONAL', subtitle:'The standard version', desc:'Lorem ipsum dolor sit amet, consectetur adipiscing elit', price:'19', currency:'$', 
+          downloads:'5 Downloads', extensions:'2 Extensions', tutorials:'Tutorials', forum:'Forum Support', updates:'1 year free updates',buttontest:'buy now', buttonlink:'#', featured:true} 
+           
+        ]
+   
+     },
+     gallery:
+     {
+       images:[
+        'gallery-image-1.jpg',
+        'gallery-image-2.jpg',
+        'gallery-image-3.jpg',
+        'gallery-image-4.jpg',
+        'gallery-image-5.jpg',
+        'gallery-image-6.jpg'
+      ]
+     },
+     footer:
+     {
+      copyrighttext:'made with love by',
+      developer:'ishika',
+      developerlink:'#'
+     },
+     socialsites:[
+     {link: 'https://www.facebook.com/',title:'facebook', target:'_blank', username:'ishika', icon:'Facebook'},
+     {link: 'https://www.google.com/',  title:'google', target:'_blank', username:'ishika', icon:'Google+'},
+     {link: 'https://www.twitter.com/', title:'twitter', target:'_blank', username:'ishika', icon:'Twitter'},
+     {link: 'https://www.instagram.com/',title:'instagram', target:'_blank', username:'ishika', icon:'Instagram'},
+     {link: 'https://www.facebook.com/', title:'behance', target:'_blank', username:'ishika', icon:'Behance'}
+    ],
+    blog:
+    {
+      tagline:'MY POETRY',
+      title:'Dive into the world of poems',
+      posts:[
+        {id:1,title:'the first poem', author:'ishika', image:'gallery-image-1.jpg', publishdate:'2011-07-14T19:43:37', excert:'this is the summary of the article'},
+        {id:2,title:'the second poem', author:'ishika', image:'gallery-image-2.jpg', publishdate:'2011-07-14T19:43:37', excert:'this is the summary of the article'},
+        {id:3,title:'the third poem', author:'ishika', image:'gallery-image-3.jpg', publishdate:'2011-07-14T19:43:37', excert:'this is the summary of the article'},
+        {id:4,title:'the fourth poem', author:'ishika', image:'gallery-image-4.jpg', publishdate:'2011-07-14T19:43:37', excert:'this is the summary1 of the article'},
+        {id:5,title:'the fifth poem', author:'ishika', image:'gallery-image-5.jpg', publishdate:'2011-07-14T19:43:37', excert:'this is the summary of the article'},
+        {id:6,title:'the sixth poem', author:'ishika', image:'gallery-image-6.jpg', publishdate:'2011-07-14T19:43:37', excert:'this is the summary of the article'},
+        {id:7,title:'the seventh poem', author:'ishika', image:'gallery-image-1.jpg', publishdate:'2011-07-14T19:43:37', excert:'this is the summary of the article'},
+        {id:8,title:'the eight poem', author:'ishika', image:'gallery-image-2.jpg', publishdate:'2011-07-14T19:43:37', excert:'this is the summary of the article'},
+        {id:9,title:'the ninth poem', author:'ishika', image:'gallery-image-3.jpg', publishdate:'2011-07-14T19:43:37', excert:'this is the summary of the article'},
+        {id:10,title:'the tenth poem', author:'ishika', image:'gallery-image-4.jpg', publishdate:'2011-07-14T19:43:37', excert:'this is the summary of the article'},
+      ]
+    }
+  };
